96b010eb97a6173c60be2530c609454e5eee7f82
Matt DeWolfe <matthewdewolfe@gmail.com>
2014-10-29 09:53:36 -0400

    Added week 9 labwork

---
+++ Week9/Generic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9
+{
+    class Generic
+    {
+        public static int ID_COUNT = 0;
+        public int ID;
+
+        public int value;
+
+        public Generic()
+        {
+            value = 0;
+            ID = ID_COUNT;
+            ID_COUNT++;
+        }
+        public Generic(int _val)
+        {
+            value = _val;
+            ID = ID_COUNT;
+            ID_COUNT++;
+        }
+
+        public static Generic operator +(Generic x, Generic y)
+        {
+            return new Generic(x.value + y.value);
+        }
+
+        public static bool operator ==(Generic x, Generic y)
+        {
+            return (x.ID == y.ID);
+        }
+        public static bool operator !=(Generic x, Generic y)
+        {
+            return (x.ID != y.ID);
+        }
+    }
+}
